fbdfb492240c3b6c4ce852b193716555d5afd401
Subhash Parwani <subhashparwani6685@gmail.com>
2023-12-05 00:54:35 +0500

    Learnt useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navigation from "./Components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
